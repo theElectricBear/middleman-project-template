93125e892596b738fce30336f578320b264358e4
Shae Kuronen <shae.kuronen@gmail.com>
2016-03-17 15:19:18 -0700

    updated gulpfile so that no longer need themes.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,24 @@
 var fs = require('fs');
 var gulp = require('gulp');
 var clean = require('gulp-clean');
+var util = require('gulp-util');
+// util.log('log this');
 
-var themes = JSON.parse(fs.readFileSync('./data/themes.json'));
+var pages = JSON.parse(fs.readFileSync('./data/pages.json'));
+
+// get unique themes in pages.json
+var themes = [];
+pages.forEach(function(page) {
+    // if themes array does not contain page.theme
+    if (page.theme && themes.indexOf(page.theme) == -1) {
+        // add page.theme to array
+        themes.push(page.theme);
+    }
+});
 
 gulp.task('clean', function () {
   themes.forEach(function(theme) {
-    return gulp.src(theme.name, {read: false})
+    return gulp.src(theme, {read: false})
       .pipe(clean());
   });
   return gulp.src('build', {read: false})
